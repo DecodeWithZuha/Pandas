964a606a49ae05b1c5b4cfbf68d75f84ae4e8884
ZuhaNasir <Decodewithzuha@gmail.com>
2026-02-07 23:59:17 +0500

    Data Extraction

--- Basics.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-
-#Pandas Series
-#Its a 1D LABELED array which can hold any type of data.
-#Series is vertical whereas Array's are Horizontal.
---
+++ Data_extraction.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+df =pd.read_csv(r'99laptop.csv')
+#print(dataframe.head())
+#print(df.columns.tolist())
+#print(df['name_c'].head(1))
+#print(df['name_c'].iloc[77])
+
+df_raw = df[['name_c', 'price']]
+print(df_raw.head())
+
+
